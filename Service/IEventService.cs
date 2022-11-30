0b1331b09a843f71447b42430a125664fa044496
Andreas Dragsberg <andr31j3@edu.zealand.dk>
2022-11-30 14:49:44 +0100

    Interface and Json started

--- Service/IEventService.cs
+++ Service/IEventService.cs
@@ -1,6 +1,17 @@
-﻿namespace BarEventPlannerV1.Service
+﻿
+using BarEventPlannerV2.Models;
+
+namespace BarEventPlannerV2.Service
 {
+    //Skrevet af Andreas Dragsberg
     public interface IEventService
     {
+        List<Event> GetEvents();
+        void CreateEvent(Event Event);
+        void UpdateEvent(Event Event);
+        void DeleteEvent(Event Event);
+
+
+
     }
 }
---
+++ Service/JSONFileEventService.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json;
+using BarEventPlannerV2.Models;
+
+namespace BarEventPlannerV2.Service
+{   
+    //Skrevet af Andreas Dragsberg og tyv stjålet fra Per Laursen.
+    public class JSONFileEventService
+    {
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        public JSONFileEventService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "Data", "Events.json"); }
+        }
+
+        public void SaveJsonEvents(List<Event> events)
+        {
+            using (FileStream jsonFileWriter = File.Create(JsonFileName))
+            {
+                Utf8JsonWriter jsonWriter = new Utf8JsonWriter(jsonFileWriter, new JsonWriterOptions()
+                {
+                    SkipValidation = false,
+                    Indented = true
+                });
+                JsonSerializer.Serialize<Event[]>(jsonWriter, events.ToArray());
+            }
+        }
+
+        public IEnumerable<Event> GetJsonEvents()
+        {
+            using (StreamReader jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Event[]>(jsonFileReader.ReadToEnd());
+            }
+        }
+    }
+
+}
