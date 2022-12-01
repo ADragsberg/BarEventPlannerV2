4ae1d3d64442b50f523aa0057e48291e8b2794b7
TrisES <112662675+TrisES@users.noreply.github.com>
2022-12-01 11:35:52 +0100

    tilføjet ekstra felt i MockData

--- MockData/MockEvents.cs
+++ MockData/MockEvents.cs
@@ -7,8 +7,8 @@ namespace BarEventPlannerV2.MockData
         private static TimeSpan wholeDay = new TimeSpan (24,0,0);
         private static List<Event> _items = new List<Event>()
         {
-            new Event(1, 20, "Børges Børnefødselsdag", true, "Børge har fødselsdag kom med gaver og få masser af lagkage", new TimeSpan(4,30,0)),
-            new Event(2, 100, "Dorits drukfest", false, "Kom alle, kom glade. Der skal drikkes", wholeDay)
+            new Event(1, 20, "Børges Børnefødselsdag", true, "Børge har fødselsdag kom med gaver og få masser af lagkage","det bliver fe", new TimeSpan(4,30,0)),
+            new Event(2, 100, "Dorits drukfest", false, "Kom alle, kom glade. Der skal drikkes","druk druk druk", wholeDay)
                     };
     }
 }
