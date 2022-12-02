000fff3bd6fa5400f785cbaeae89ffa88e610e99
TrisES <112662675+TrisES@users.noreply.github.com>
2022-12-02 14:47:06 +0100

    TODO kommentar i Event.cs

--- Models/Event.cs
+++ Models/Event.cs
@@ -5,6 +5,7 @@ using System.Xml.Linq;
 namespace BarEventPlannerV2.Models
 {
     // TODO: Skal nok have flere properties/felter i fremtiden.
+    // TODO: I fremtiden skal vi nok lave, at Event har en Kunde some felt/prop eller reference via. ID.
 
     /// <summary>
     /// Base klasse for alle events (ikke abstrakt endnu).
