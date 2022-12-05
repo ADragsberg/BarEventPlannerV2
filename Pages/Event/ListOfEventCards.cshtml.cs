12db5a79961112991567d9658e6bba0e33bfd9ad
TrisES <112662675+TrisES@users.noreply.github.com>
2022-12-05 09:49:08 +0100

    Start på at bruge et 'Partial View'. Har lavet et Event kort og forbundet 'dataflowet'. Event kortet skal 'smukseres' og laves færdig, som vi vil have det.

---
+++ Pages/Event/ListOfEventCards.cshtml.cs
@@ -0,0 +1,30 @@
+using BarEventPlannerV2.Service.Repositories.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BarEventPlannerV2.Pages.Event
+{
+    public class ListOfEventCardsModel : PageModel
+    {
+		private IEventRepository _eventRepository;
+		private IKundeRepository _kundeRepository;
+
+		// Constructor with EventService dependency injection
+		public ListOfEventCardsModel(IEventRepository eventRepository, IKundeRepository kundeRepository)
+		{
+			_eventRepository = eventRepository;
+			_kundeRepository = kundeRepository;
+		}
+
+		[BindProperty]
+		public List<@Models.Event> Events { get; set; }
+		
+
+
+		public void OnGet()
+        {
+			Events = _eventRepository.GetAll().ToList();
+		}
+    }
+	
+}
