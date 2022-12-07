0a94772a16b9e88c8b1c5edea02a3258c4262eec
MikkelCH <mikk320i@edu.zealand.dk>
2022-12-07 13:10:23 +0100

    Lavet page for godkend. mangler funktion

--- Pages/Event/GodkendEvent.cshtml.cs
+++ Pages/Event/GodkendEvent.cshtml.cs
@@ -1,12 +1,50 @@
+using BarEventPlannerV2.Models;
+using BarEventPlannerV2.Service.Repositories.Interface;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Xml.Linq;
 
 namespace BarEventPlannerV2.Pages.Event
 {
     public class GodkendEventModel : PageModel
     {
-        public void OnGet()
+        private IEventRepository _eventRepository;
+        private IKundeRepository _kundeRepository;
+
+        public GodkendEventModel(IEventRepository eventRepository, IKundeRepository kundeRepository)
         {
+            _eventRepository = eventRepository;
+            _kundeRepository = kundeRepository;
+
         }
+
+        [BindProperty]
+        public Models.Kunde Kunde { get; set; }
+        [BindProperty]
+        public Models.Event Event { get; set; }
+
+        public IActionResult OnGet(int id)
+        {
+            Event = _eventRepository.Read(id);
+            Kunde = _kundeRepository.Read(id);
+            if (Event == null)
+                return RedirectToPage("/NotFound"); //NotFound er ikke defineret endnu
+
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            //if (!ModelState.IsValid)
+            //{
+            //    return Page();
+            //}
+            //else
+            Event.Godkendt = true;
+            _eventRepository.Update(Event.Id, Event);
+            return RedirectToPage("/Event/ListOfEventCards");
+        }
+
+
     }
 }
