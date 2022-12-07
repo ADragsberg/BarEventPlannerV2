0594cac35ec6913b1b1f4c2007e14f1227f3243f
TrisES <112662675+TrisES@users.noreply.github.com>
2022-12-07 14:58:52 +0100

    Lavet et Partial View med genbrugelige dele af vores 'hoved form'. Samt brugt det Partial View i en OpretEvent Razor Page, som sætter et Event til at være godkendt med det samme.

---
+++ Pages/Event/OpretEvent.cshtml.cs
@@ -0,0 +1,47 @@
+using BarEventPlannerV2.Service.Repositories.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BarEventPlannerV2.Pages.Event
+{
+    public class OpretEventModel : PageModel
+    {
+		// Felter
+		private readonly IEventRepository _eventRepository;
+        private readonly IKundeRepository _kundeRepository;
+
+        // Konstruktør
+        public OpretEventModel(IEventRepository eventRepository, IKundeRepository kundeRepository)
+        {
+            _eventRepository = eventRepository;
+            _kundeRepository = kundeRepository;
+        }
+
+		// Props
+		[BindProperty]
+		public Models.Event Event { get; set; }
+        [BindProperty]
+        public Models.Kunde Kunde { get; set; }
+
+
+        public IActionResult OnGet()
+        {
+			return Page();
+		}
+
+		public IActionResult OnPost()
+		{
+			Event.Kunde = Kunde;
+			if (!ModelState.IsValid)
+			{
+				return Page();
+			}
+			Event.Godkendt = true;
+
+            _kundeRepository.Create(Kunde);
+            _eventRepository.Create(Event);
+			return RedirectToPage("/Event/EventBetalt");
+		}
+
+	}
+}
